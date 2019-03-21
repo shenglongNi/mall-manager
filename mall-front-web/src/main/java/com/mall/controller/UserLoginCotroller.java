512488ec3b80e899a832cf0b1a99d084d84728c6
shenglongNi <shenglongni@163.com>
2019-03-21 17:12:26 +0800

    登录验证

--- mall-front-web/src/main/java/com/mall/controller/UserLoginCotroller.java
+++ mall-front-web/src/main/java/com/mall/controller/UserLoginCotroller.java
@@ -1,11 +1,13 @@
 package com.mall.controller;
 
+import com.google.common.collect.Maps;
+import com.mall.dto.CommonResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 import org.springframework.security.authentication.encoding.PasswordEncoder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -16,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Map;
+
 
 @Controller
 public class UserLoginCotroller {
@@ -25,13 +29,14 @@ public class UserLoginCotroller {
     private AuthenticationManager authenticationManager;
 
     @Autowired
-    private PasswordEncoder passwordEncoder;
+    private Md5PasswordEncoder passwordEncoder;
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     @ResponseBody
-    public String login(@RequestParam("username") String username, @RequestParam("password") String password) {
+    public Object login(@RequestParam("username") String username, @RequestParam("password") String password) {
 
-
+        Map<String, Object> dataMap = Maps.newHashMap();
+        CommonResult result = new CommonResult();
         UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToke
                 = new UsernamePasswordAuthenticationToken(username, passwordEncoder.encodePassword(password, null));
 
@@ -44,6 +49,6 @@ public class UserLoginCotroller {
         } catch (AuthenticationException e) {
             logger.error(e.getMessage(), e);
         }
-        return null;
+        return result.success(dataMap);
     }
 }
